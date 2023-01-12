4d833b8be3a9c5cab7f2a6fa2dbd83b9929eac57
Ahmet Fatih Çenesiz <fcenesiz@gmail.com>
2023-01-12 15:31:46 +0300

    Initial commit

---
+++ app/src/main/java/com/fcenesiz/composesplashscreen/Navigation.kt
@@ -0,0 +1,86 @@
+package com.fcenesiz.composesplashscreen
+
+import android.view.animation.OvershootInterpolator
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
+
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+    NavHost(
+        navController = navController,
+        startDestination = Screen.SplashScreen.route
+    ) {
+        composable(Screen.SplashScreen.route) {
+            SplashScreen(navController = navController)
+        }
+        composable(Screen.MainScreen.route) {
+            MainScreen()
+        }
+    }
+}
+
+@Composable
+fun SplashScreen(
+    navController: NavController
+) {
+    val scale = remember {
+        Animatable(0f)
+    }
+    LaunchedEffect(key1 = true) {
+        scale.animateTo(
+            targetValue = 0.3f,
+            animationSpec = tween(
+                durationMillis = 500,
+                easing = {
+                    OvershootInterpolator(2f).getInterpolation(it)
+                }
+            )
+        )
+        delay(3000L)
+        navController.navigate(Screen.MainScreen.route)
+    }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = "icon",
+            modifier = Modifier
+                .scale(scale.value)
+                .size(500.dp)
+        )
+    }
+}
+
+@Composable
+fun MainScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "MAIN SCREEN UI", color = Color.White)
+    }
+}
---
+++ app/src/main/java/com/fcenesiz/composesplashscreen/Screen.kt
@@ -0,0 +1,15 @@
+package com.fcenesiz.composesplashscreen
+
+sealed class Screen(val route: String){
+    object SplashScreen : Screen("splash_screen")
+    object MainScreen : Screen("main_screen")
+
+    fun withArgs(vararg args: String): String {
+        return buildString {
+            append(route)
+            args.forEach { arg ->
+                append("/$arg")
+            }
+        }
+    }
+}
